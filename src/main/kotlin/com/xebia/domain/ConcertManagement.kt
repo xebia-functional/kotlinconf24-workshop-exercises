71a9aa256689fccf0bd8aa9059a5bfc6e64c3bc6
franciscodr <francisco.diaz@xebia.com>
2024-05-21 19:17:46 +0200

    Add more information to the concert management exercise

--- src/main/kotlin/com/xebia/domain/ConcertManagement.kt
+++ src/main/kotlin/com/xebia/domain/ConcertManagement.kt
@@ -2,6 +2,19 @@ package com.xebia.com.xebia.domain
 
 import java.time.LocalDate
 
+/**
+ * In this exercise, we will implement a naive event/state application to simulate how to manage
+ * a [Concert]. To do so, we need to implement [ConcertManagementState.processAction] by using
+ * pattern matching.
+ *
+ * Some aspects to consider:
+ * - In the action doesn't have effect on the current state of the concert, we'll return the current state.
+ * - Take into account that users might want to buy more tickets than are available! If there's not enough
+ *   tickets, the state won't change. We can revisit this behavior later, once we explain typed errors.
+ * - If there are still pre-sales tickets left when the general sales is open, we should sum them to the
+ *   total general tickets available.
+ * - We'll only hang the Sold Out letter when someone buy exactly the number of tickets available.
+ */
 sealed interface ConcertManagementAction {
     data class AnnounceNewDate(val concertDate: LocalDate) : ConcertManagementAction
     data class OpenPresales(val presalesTicketsCount: Int) : ConcertManagementAction
@@ -29,14 +42,24 @@ sealed interface ConcertManagementState {
     }
 }
 
-fun simulateActivity(
-    initialState: ConcertManagementState,
-    actions: List<ConcertManagementAction>
-): ConcertManagementState {
-    return actions.fold(initialState) { state, action -> state.processAction(action) }
+data class Concert(val state: ConcertManagementState) {
+    fun simulateActivity(
+        actions: List<ConcertManagementAction>
+    ): Concert {
+        val finalState = actions.fold(state) { state, action -> state.processAction(action) }
+        return copy(state = finalState)
+    }
+
+    companion object {
+        fun announceNewConcert(date: LocalDate): Concert =
+            Concert(ConcertManagementState.ConcertAnnounced(date))
+    }
 }
 
 fun main() {
+    val firstConcertDate = LocalDate.now().plusMonths(2)
+    val secondConcertDate = firstConcertDate.plusDays(1)
+
     val actions = listOf(
         ConcertManagementAction.OpenPresales(10),
         ConcertManagementAction.SellTickets(2),
@@ -51,9 +74,17 @@ fun main() {
         ConcertManagementAction.SellTickets(4),
         ConcertManagementAction.SellTickets(4),
         ConcertManagementAction.SellTickets(3),
-        ConcertManagementAction.AnnounceNewDate(LocalDate.now()),
+        ConcertManagementAction.AnnounceNewDate(secondConcertDate),
     )
 
-    simulateActivity(ConcertManagementState.ConcertAnnounced(LocalDate.now()), actions)
-}
+    val concertAfterSimulation = Concert
+        .announceNewConcert(firstConcertDate)
+        .simulateActivity(actions)
 
+    assert(
+        concertAfterSimulation.state is ConcertManagementState.ConcertAnnounced
+                && concertAfterSimulation.state.releaseDate == secondConcertDate
+    )
+
+    println(concertAfterSimulation)
+}
